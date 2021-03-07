f6dbd10db419a51327ba89961012af9e9ea53b75
SlavishaStojanoski <76256564+SlavishaStojanoski@users.noreply.github.com>
2021-03-07 16:33:17 +0100

    send mail using nodemailer

---
+++ Email/Email.js
@@ -0,0 +1,60 @@
+require('../node_modules/dotenv').config()
+
+const events = require('events');
+const emitter = new events.EventEmitter();
+
+const nodemailer = require('nodemailer');
+
+//Transporter
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+});
+
+//Mail option.. from,to,headline,content...
+const createdMail = {
+    from: 'nnodejs@gmail.com',
+    to: 'slavisa.stojanoski992@gmail.com',
+    subject: 'New product!',
+    text: 'This is content for the product created event'
+}
+//Second mail option
+const deleteInfoMail = {
+    from: 'nnodejs@gmail.com',
+    to: 'slavisa.stojanoski992@gmail.com',
+    subject: 'Oh no! Product deleted!',
+    text: "Don't over-React.js"
+}
+
+
+
+// this emmiter sent mail with subject:NEW product! and 
+//content 'This is content for the product created event'
+emitter.on('product_created', data => {
+    //create(send mail) with transporter
+    transporter.sendMail(createdMail, (err, data) => {
+        if(err) {
+            console.log('Error', err)
+        } else {
+            console.log('Email sent!!')
+        }
+    } )
+})
+
+// this emmiter sent mail with subject:NEW product! and 
+//content 'This is content for the product created event'
+emitter.on('product_deleted', data => {
+    //create(send mail) with transporter
+    transporter.sendMail(deleteInfoMail, (err, data) => {
+        if(err) {
+            console.log('Error', err)
+        } else {
+            console.log('Email sent!!')
+        }
+    } )
+})
+
+emitter.emit('product_deleted')
