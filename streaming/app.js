56545c4b95ad3a90a01280075e07f647cd9b1e03
SlavishaStojanoski <76256564+SlavishaStojanoski@users.noreply.github.com>
2021-03-19 23:29:48 +0100

    streaming

---
+++ streaming/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+app
+    .get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+    })
+    .get('/video', (req, res) => {
+        const range = req.headers.range;
+        if (!range) {
+            res.status(400).send('Require Range Header')
+        }
+    
+
+    const videoPath = 'video.mp4'
+    const videoSize = fs.statSync(videoPath).size
+
+    const CHUNK_SIZE = 10 ** 5 // 
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
+
+    const contentLength = end - start + 1
+    const headers = {
+        'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': contentLength,
+        'Content-Type': 'video.mp4'
+    } 
+    res.writeHead(206, headers)
+
+    const videoStream = fs.createReadStream(videoPath, {start, end})
+    
+    videoStream.pipe(res)
+    })
+
+app.listen(3000, ()=> { console.log('Listening on port 3000') })    
